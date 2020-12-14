83f696c9548ccf35501672ca84bc40d66fe889c6
csunklee <65987216+csunklee@users.noreply.github.com>
2020-12-14 12:17:58 -0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+/**This is based off of Navbar.js from https://github.com/briancodex/react-website-v1.git**/
 import React, { useState, useEffect } from 'react';
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
